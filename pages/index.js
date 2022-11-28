7ad629c09ede8a49018ee52bf890ce426aac391a
Thomas Vu <thomasjvu@gmail.com>
2022-11-28 01:27:57 -0500

    Add latest build

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,113 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import React from "react";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert your headline here</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
-          </div>
+    const [userInput, setUserInput] = useState("");
+    const [apiOutput, setApiOutput] = useState("");
+    const [isGenerating, setIsGenerating] = useState(false);
+
+    const callGenerateEndpoint = async () => {
+        setIsGenerating(true);
+
+        console.log("Calling OpenAI...");
+        const response = await fetch("/api/generate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userInput }),
+        });
+
+        const data = await response.json();
+        const { output } = data;
+        console.log("OpenAI replied...", output.text);
+
+        setApiOutput(`${output.text}`);
+        setIsGenerating(false);
+    };
+
+    const onUserChangedText = (event) => {
+        console.log(event.target.value);
+        setUserInput(event.target.value);
+    };
+
+    return (
+        <div className="root">
+            <Head>
+                <title>Indie Game Blurb Generator</title>
+            </Head>
+            <div className="container">
+                <div className="header">
+                    <div className="header-title">
+                        <h1>Generate Game Blurb</h1>
+                    </div>
+                    <div className="header-subtitle">
+                        <h2>
+                            Write out some details of your game and receive a
+                            captivating synopsis.
+                            <span className="header-example">
+                                (Ex. Title, genre, items, plot, character names,
+                                multiplayer, systems, etc.)
+                            </span>
+                        </h2>
+                    </div>
+                </div>
+                <div className="prompt-container">
+                    <textarea
+                        className="prompt-box"
+                        placeholder="Skylight is a farming and romance simulator set in a sky city. Your decisions matter!"
+                        value={userInput}
+                        onChange={onUserChangedText}
+                    ></textarea>
+                    <div className="prompt-buttons">
+                        <a
+                            className={
+                                isGenerating
+                                    ? "generate-button loading"
+                                    : "generate-button"
+                            }
+                            onClick={callGenerateEndpoint}
+                        >
+                            <div className="generate">
+                                {isGenerating ? (
+                                    <span class="loader"></span>
+                                ) : (
+                                    <p>Generate</p>
+                                )}
+                            </div>
+                        </a>
+                    </div>
+                    {apiOutput && (
+                        <div className="output">
+                            <div className="output-header-container">
+                                <div className="output-header">
+                                    <h3>Output</h3>
+                                </div>
+                            </div>
+                            <div className="output-content">
+                                <p>{apiOutput}</p>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <div className="badge-container grow">
+                <a
+                    href="https://buildspace.so/builds/ai-writer"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <div className="badge">
+                        <Image src={buildspaceLogo} alt="buildspace logo" />
+                        <p>build with buildspace</p>
+                    </div>
+                </a>
+            </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
